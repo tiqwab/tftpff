e80a92b03360d3054900b58a397b9cd2a1c32760
tiqwab <tiqwab.ch90@gmail.com>
2021-12-29 11:31:32 +0900

    Move main logic to server

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,26 @@
 mod packet;
-mod temp_dir;
 
 use anyhow::{bail, Context, Result};
 use log::{debug, error, warn};
-use std::fs;
-use std::io::{Read, Write};
-use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
-use std::path::{Path, PathBuf};
+use std::net::Ipv4Addr;
+use std::path::Path;
 use std::str::FromStr;
+use tftpff::server;
+use tftpff::temp_dir;
 
 fn main() -> Result<()> {
     env_logger::init();
 
-    // FIXME: Extract below as TftpServer struct
     let server_addr = Ipv4Addr::from_str("0.0.0.0")?;
     let server_port = 12345;
     let base_dir = Path::new("/tmp/tftpff");
     let temp_dir = temp_dir::create_temp_dir()?;
 
-    let sock_addr = SocketAddr::from((server_addr, server_port));
-    let sock = UdpSocket::bind(sock_addr).context("Failed to bind")?;
-    let mut buf = [0; 1024];
-
-    debug!("listening at {}:{}", server_addr, server_port);
-
-    loop {
-        let (n, client_addr) = sock.recv_from(&mut buf)?;
-        if n == 0 {
-            break;
-        }
-
-        let raw = &buf[..n];
-
-        match packet::InitialPacket::parse(raw) {
-            Ok(packet::InitialPacket::WRQ(wrq)) => {
-                debug!("received WRQ: {:?}", wrq);
-                let mut block = 0;
-                let ack = packet::ACK::new(block);
-                sock.send_to(&ack.encode(), client_addr)?;
-                block += 1;
-                debug!("sent ack: {:?}", ack);
-
-                let mut temp_file_path = PathBuf::from(temp_dir.path());
-                temp_file_path.push(&wrq.filename);
-                let mut temp_file = fs::File::create(&temp_file_path)?;
-                debug!("created {:?}", temp_file_path);
-
-                loop {
-                    let (n, client_addr) = sock.recv_from(&mut buf)?;
-                    let raw = &buf[..n];
-                    match packet::Data::parse(raw) {
-                        Ok(pkt) => {
-                            debug!("received data: size={}", pkt.data().len());
-                            if pkt.data().len() == 0 {
-                                break;
-                            }
-                            temp_file.write(pkt.data())?;
-                            let ack = packet::ACK::new(block);
-                            sock.send_to(&ack.encode(), client_addr)?;
-                            block += 1;
-                            debug!("sent ack: {:?}", ack);
-
-                            if pkt.data().len() < 512 {
-                                break;
-                            }
-                        }
-                        Err(err) => {
-                            todo!()
-                        }
-                    }
-                }
-
-                let mut dest_path = PathBuf::from(base_dir);
-                dest_path.push(&wrq.filename);
-                fs::rename(temp_file_path, dest_path)?;
-                debug!("finish WRQ for {:?}", wrq.filename);
-            }
-            Ok(packet::InitialPacket::RRQ(rrq)) => {
-                debug!("received RRQ: {:?}", rrq);
-                let mut block = 1;
-
-                let mut src_path = PathBuf::from(base_dir);
-                src_path.push(&rrq.filename);
-                let mut file = fs::File::open(&src_path)?; // FIXME: error handling
-
-                loop {
-                    let mut file_buf = [0 as u8; 512];
-                    let file_n = file.read(&mut file_buf)?;
-                    let data_pkt = packet::Data::new(block, &file_buf[..file_n]);
-                    sock.send_to(&data_pkt.encode(), client_addr)?;
-
-                    let (ack_n, client_addr) = sock.recv_from(&mut buf)?;
-                    match packet::ACK::parse(&buf[..ack_n]) {
-                        Ok(pkt) => {
-                            if pkt.block() != block {
-                                warn!("received ACK with wrong block.")
-                                // TODO: resend
-                            }
-                        }
-                        Err(err) => {
-                            warn!("couldn't receive ACK: {:?}", err)
-                            // TODO: resend
-                        }
-                    }
-                    block += 1;
-                    debug!("sent data: size={}", file_n);
-                    if file_n < 512 {
-                        break;
-                    }
-                }
-
-                debug!("finish RRQ for {:?}", rrq.filename);
-            }
-            Err(err) => {
-                bail!("Failed to parse InitialPacket: {:?}", err);
-            }
-        }
-    }
+    let mut server =
+        server::TftpServer::create(server_addr, server_port, base_dir, temp_dir.path())
+            .context("Failed to create TftpServer")?;
+    server.bind().context("Failed to bind")?;
+    server.run().context("Failed in TftpServer running")?;
 
     return Ok(());
 }
--- src/server.rs
+++ src/server.rs
@@ -1,34 +1,50 @@
-use crate::packet;
 use crate::packet::{ReadPacket, WritePacket};
+use crate::{packet, temp_dir};
 use anyhow::{Context, Result};
 use log::{debug, error, warn};
+use std::io::{Read, Write};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
-use std::thread;
 use std::thread::JoinHandle;
+use std::{fs, thread};
 
 pub struct TftpServer {
     server_addr: Ipv4Addr,
     server_port: u16,
-    base_dir: PathBuf,
-    rrq_handler: Arc<Box<dyn Fn(UdpSocket, ReadPacket) -> () + Send + Sync>>,
-    wrq_handler: Arc<Box<dyn Fn(UdpSocket, WritePacket) -> () + Send + Sync>>,
+    rrq_handler: Arc<Box<dyn Fn(UdpSocket, SocketAddr, ReadPacket) -> Result<()> + Send + Sync>>,
+    wrq_handler: Arc<Box<dyn Fn(UdpSocket, SocketAddr, WritePacket) -> Result<()> + Send + Sync>>,
     server_sock: Option<UdpSocket>,
 }
 
 impl TftpServer {
-    pub fn new(
+    pub fn create(
         server_addr: Ipv4Addr,
         server_port: u16,
-        base_dir: PathBuf,
-        rrq_handler: Box<dyn Fn(UdpSocket, ReadPacket) -> () + Send + Sync>,
-        wrq_handler: Box<dyn Fn(UdpSocket, WritePacket) -> () + Send + Sync>,
+        base_dir: impl AsRef<Path>,
+        temp_dir: impl AsRef<Path>,
+    ) -> Result<TftpServer> {
+        let rrq_handler = create_rrq_handler(base_dir.as_ref().to_owned());
+        let wrq_handler =
+            create_wrq_handler(base_dir.as_ref().to_owned(), temp_dir.as_ref().to_owned());
+        Ok(TftpServer {
+            server_addr,
+            server_port,
+            rrq_handler: Arc::new(Box::new(rrq_handler)),
+            wrq_handler: Arc::new(Box::new(wrq_handler)),
+            server_sock: None,
+        })
+    }
+
+    pub fn create_with_handlers(
+        server_addr: Ipv4Addr,
+        server_port: u16,
+        rrq_handler: Box<dyn Fn(UdpSocket, SocketAddr, ReadPacket) -> Result<()> + Send + Sync>,
+        wrq_handler: Box<dyn Fn(UdpSocket, SocketAddr, WritePacket) -> Result<()> + Send + Sync>,
     ) -> TftpServer {
         TftpServer {
             server_addr,
             server_port,
-            base_dir,
             rrq_handler: Arc::new(rrq_handler),
             wrq_handler: Arc::new(wrq_handler),
             server_sock: None,
@@ -63,7 +79,7 @@ impl TftpServer {
                 Ok(packet::InitialPacket::WRQ(wrq)) => {
                     match UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)) {
                         Ok(child_sock) => {
-                            let h = self.spawn_wrq(child_sock, wrq);
+                            self.spawn_wrq(child_sock, client_addr, wrq);
                         }
                         Err(err) => {
                             error!("Failed to create child_sock for {:?}. {:?}", wrq, err);
@@ -73,7 +89,7 @@ impl TftpServer {
                 Ok(packet::InitialPacket::RRQ(rrq)) => {
                     match UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)) {
                         Ok(child_sock) => {
-                            self.spawn_rrq(child_sock, rrq);
+                            self.spawn_rrq(child_sock, client_addr, rrq);
                         }
                         Err(err) => {
                             error!("Failed to create child_sock for {:?}. {:?}", rrq, err);
@@ -87,14 +103,124 @@ impl TftpServer {
         }
     }
 
-    fn spawn_rrq(&self, socket: UdpSocket, rrq: ReadPacket) -> JoinHandle<()> {
+    fn spawn_rrq(
+        &self,
+        socket: UdpSocket,
+        client_addr: SocketAddr,
+        rrq: ReadPacket,
+    ) -> JoinHandle<()> {
         let handler = Arc::clone(&self.rrq_handler);
-        thread::spawn(move || (handler)(socket, rrq))
+        thread::spawn(move || {
+            (handler)(socket, client_addr, rrq).unwrap_or_else(|err| {
+                error!("Failed in handling RRQ from {}: {:?}", client_addr, err)
+            })
+        })
     }
 
-    fn spawn_wrq(&self, socket: UdpSocket, wrq: WritePacket) -> JoinHandle<()> {
+    fn spawn_wrq(
+        &self,
+        socket: UdpSocket,
+        client_addr: SocketAddr,
+        wrq: WritePacket,
+    ) -> JoinHandle<()> {
         let handler = Arc::clone(&self.wrq_handler);
-        thread::spawn(move || (handler)(socket, wrq))
+        thread::spawn(move || {
+            (handler)(socket, client_addr, wrq).unwrap_or_else(|err| {
+                error!("Failed in handling WRQ from {}: {:?}", client_addr, err)
+            })
+        })
+    }
+}
+
+pub fn create_rrq_handler(
+    base_dir: PathBuf,
+) -> impl Fn(UdpSocket, SocketAddr, ReadPacket) -> Result<()> {
+    move |sock, client_addr, rrq| {
+        debug!("received RRQ: {:?}", rrq);
+        let mut block = 1;
+        let mut buf = [0; 1024];
+
+        let src_path = base_dir.join(&rrq.filename);
+        let mut file = fs::File::open(&src_path)?; // FIXME: error handling
+
+        loop {
+            let mut file_buf = [0 as u8; 512];
+            let file_n = file.read(&mut file_buf)?;
+            let data_pkt = packet::Data::new(block, &file_buf[..file_n]);
+            sock.send_to(&data_pkt.encode(), client_addr)?;
+
+            let (ack_n, client_addr) = sock.recv_from(&mut buf)?;
+            match packet::ACK::parse(&buf[..ack_n]) {
+                Ok(pkt) => {
+                    if pkt.block() != block {
+                        warn!("received ACK with wrong block.")
+                        // TODO: resend
+                    }
+                }
+                Err(err) => {
+                    warn!("couldn't receive ACK: {:?}", err)
+                    // TODO: resend
+                }
+            }
+            block += 1;
+            debug!("sent data: size={}", file_n);
+            if file_n < 512 {
+                break;
+            }
+        }
+
+        debug!("finish RRQ for {:?}", rrq.filename);
+        return Ok(());
+    }
+}
+
+pub fn create_wrq_handler(
+    base_dir: PathBuf,
+    temp_dir: PathBuf,
+) -> impl Fn(UdpSocket, SocketAddr, WritePacket) -> Result<()> {
+    move |sock, client_addr, wrq| {
+        debug!("received WRQ: {:?}", wrq);
+        let mut block = 0;
+        let mut buf = [0; 1024];
+
+        let ack = packet::ACK::new(block);
+        sock.send_to(&ack.encode(), client_addr)?;
+        block += 1;
+        debug!("sent ack: {:?}", ack);
+
+        let temp_file_path = temp_dir.join(&wrq.filename);
+        let mut temp_file = fs::File::create(&temp_file_path)?;
+        debug!("created {:?}", temp_file_path);
+
+        loop {
+            let (n, client_addr) = sock.recv_from(&mut buf)?;
+            let raw = &buf[..n];
+            match packet::Data::parse(raw) {
+                Ok(pkt) => {
+                    debug!("received data: size={}", pkt.data().len());
+                    if pkt.data().len() == 0 {
+                        break;
+                    }
+                    temp_file.write(pkt.data())?;
+                    let ack = packet::ACK::new(block);
+                    sock.send_to(&ack.encode(), client_addr)?;
+                    block += 1;
+                    debug!("sent ack: {:?}", ack);
+
+                    if pkt.data().len() < 512 {
+                        break;
+                    }
+                }
+                Err(err) => {
+                    todo!()
+                }
+            }
+        }
+
+        let dest_path = base_dir.join(&wrq.filename);
+        fs::rename(temp_file_path, dest_path)?;
+        debug!("finish WRQ for {:?}", wrq.filename);
+        return Ok(());
     }
 }
 
@@ -118,17 +244,18 @@ mod tests {
             let rq = Arc::clone(&rrq_queue);
             let wq = Arc::clone(&wrq_queue);
 
-            let rrq_handler = move |sock, pkt| {
+            let rrq_handler = move |sock, addr, pkt| {
                 rq.lock().unwrap().push(pkt);
+                Ok(())
             };
-            let wrq_handler = move |sock, pkt| {
+            let wrq_handler = move |sock, addr, pkt| {
                 wq.lock().unwrap().push(pkt);
+                Ok(())
             };
 
-            let mut server = TftpServer::new(
+            let mut server = TftpServer::create_with_handlers(
                 Ipv4Addr::from_str("127.0.0.1")?,
                 0,
-                temp_dir.path().to_owned(),
                 Box::new(rrq_handler),
                 Box::new(wrq_handler),
             );
