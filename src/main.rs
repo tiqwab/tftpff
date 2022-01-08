25dfeb471262cd8435bb7d2c24cd5170b8641b6f
tiqwab <tiqwab.ch90@gmail.com>
2022-01-08 13:02:52 +0900

    Accept variables from command options

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use anyhow::{Context, Result};
+use clap::Parser;
 use std::net::Ipv4Addr;
-use std::path::Path;
+use std::path::PathBuf;
 use std::str::FromStr;
 use tftpff::server;
 use tftpff::temp;
 
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+    #[clap(short, long)]
+    dir: String,
+
+    #[clap(short, long, default_value = "0.0.0.0")]
+    addr: String,
+
+    #[clap(short, long, default_value_t = 69)]
+    port: u16,
+}
+
 fn main() -> Result<()> {
     env_logger::init();
 
-    let server_addr = Ipv4Addr::from_str("0.0.0.0")?;
-    let server_port = 12345;
-    let base_dir = Path::new("/tmp/tftpff");
+    let args = Args::parse();
+
+    let server_addr = Ipv4Addr::from_str(&args.addr)?;
+    let server_port: u16 = args.port;
+    let base_dir = PathBuf::from_str(&args.dir)?;
     let temp_dir = temp::create_temp_dir()?;
 
     let mut server = server::TftpServer::create(
